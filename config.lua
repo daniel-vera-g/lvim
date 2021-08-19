2599b09a90c25160faa46262d8b41965d7bde369
danielVg <danielveragi@gmail.com>
2021-08-19 13:51:17 +0200

    Migrate old built in vimrc config and some plugins

--- config.lua
+++ config.lua
@@ -1,7 +1,9 @@
 -- LunarVim internal settings
 lvim.format_on_save = true
 lvim.lint_on_save = true
-lvim.colorscheme = "tokyonight"
+lvim.colorscheme = "spacegray"
+lvim.encoding="utf-8"
+lvim.fileencoding="utf-8"
 
 -- Vim built in
 vim.opt.cmdheight = 1
@@ -9,8 +11,19 @@ vim.opt.relativenumber = true
 vim.opt.wrap = true
 vim.opt.spell = false
 
+-- Spaces for tabs
+vim.opt.tabstop=2 -- number of spaces used as tab for file(Number of visual spaces per TAB)
+vim.opt.softtabstop=2 -- number of spaces used as tab for editing(Number of spaces in TAB when editing)
+vim.opt.shiftwidth=2 -- number of spaces used to autoindent(Number of spaces indented when reindent operations (>> and <<) are used)
+vim.opt.expandtab=true -- expand tabs into spaces(Convert TABs to spaces)
+vim.opt.smarttab=true -- smart tabulation and backspace(Enable intelligent tabbing and spacing for indentation and alignment)
+vim.opt.bs="indent,eol,start" -- allow backspacing over everything
+vim.opt.ai=true -- Auto indent
+
+
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = ","
+vim.api.nvim_set_keymap('n', 'Y', 'yg_', {noremap=true})
 
 -- +++
 -- Plugin configs
@@ -23,7 +36,7 @@ lvim.builtin.terminal.active = true
 -- nvimtree
 lvim.builtin.nvimtree.side = "right"
 lvim.builtin.nvimtree.show_icons.git = 0
-lvim.builtin.nvimtree.hide_dotfiles = 1
+lvim.builtin.nvimtree.hide_dotfiles = 0
 
 -- Treesitter parsers
 lvim.builtin.treesitter.ensure_installed = {}
@@ -39,6 +52,8 @@ lvim.plugins = {
 
   -- Utils
   {"Pocco81/AutoSave.nvim"},
+  {"tpope/vim-surround"},
+  {'machakann/vim-highlightedyank'},
   -- TODO gx open links
   {"felipec/vim-sanegx",
     event = "BufRead"
@@ -73,5 +88,26 @@ lvim.plugins = {
       vim.g.mkdp_auto_start = 1
     end,
   },
+
+  -- Git
+  {
+  "tpope/vim-fugitive",
+  cmd = {
+    "G",
+    "Git",
+    "Gdiffsplit",
+    "Gread",
+    "Gwrite",
+    "Ggrep",
+    "GMove",
+    "GDelete",
+    "GBrowse",
+    "GRemove",
+    "GRename",
+    "Glgrep",
+    "Gedit"
+  },
+  ft = {"fugitive"}
+},
 }
 
