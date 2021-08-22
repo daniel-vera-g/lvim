1776be662384f2c7e795afa0271feac802a0db66
danielVg <danielveragi@gmail.com>
2021-08-22 23:26:52 +0200

    vsnips config for custom snippets

---
+++ lua/user/plugins/vsnips.lua
@@ -0,0 +1,3 @@
+lvim.vsnip_dir = "~/.config/lvim/snippets/"
+vim.api.nvim_set_keymap('i', '<C-l>', "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", {expr = true})
+vim.api.nvim_set_keymap('s', '<C-l>', "vsnip#available(1)  ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>'", {expr = true})
