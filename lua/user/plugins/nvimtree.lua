869c9e84d7dcfe2cc9e1972c5148b533b11827a9
danielVg <danielveragi@gmail.com>
2021-08-22 23:25:07 +0200

    Fix gx bug?...Probably not

--- lua/user/plugins/nvimtree.lua
+++ lua/user/plugins/nvimtree.lua
@@ -1,4 +1,5 @@
 lvim.builtin.nvimtree.side = "right"
 lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.nvimtree.hide_dotfiles = 0
-
+lvim.builtin.nvimtree.nvim_tree_disable_netrw = 0
+lvim.builtin.nvimtree.nvim_tree_hijack_netrw = 0
