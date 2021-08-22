0f504e9a3a1cabe53ce64a4107e24933cbbc47c3
danielVg <danielveragi@gmail.com>
2021-08-22 23:24:50 +0200

    Mooore plugins

--- lua/user/plugins/init.lua
+++ lua/user/plugins/init.lua
@@ -3,8 +3,11 @@ return {
   nvimtree = require('user.plugins.nvimtree'),
   treesitter = require('user.plugins.treesitter'),
   vim_markdown = require('user.plugins.vim_markdown'),
-  dashboard = require('user.plugins.dashboard'),
   terminal = require('user.plugins.terminal'),
   fugitive = require('user.plugins.fugitive'),
-  dashboard = require('user.plugins.dashboard')
+  dashboard = require('user.plugins.dashboard'),
+  vsnips = require('user.plugins.vsnips'),
+  compe = require('user.plugins.compe'),
+  wiki = require("user.plugins.wiki"),
+  autosave = require('user.plugins.autosave'),
 }
