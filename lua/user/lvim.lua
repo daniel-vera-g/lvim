5965e92dc29acf198500e6f0ec48a688b6f03605
danielVg <danielveragi@gmail.com>
2021-08-27 10:05:52 +0200

    refactor(config): move config parts to it's respective files/components

---
+++ lua/user/lvim.lua
@@ -0,0 +1,12 @@
+lvim.format_on_save = true
+lvim.lint_on_save = true
+
+lvim.colorscheme = "catppuccino"
+require("user.plugins.catppuccino")
+
+lvim.encoding="utf-8"
+lvim.fileencoding="utf-8"
+
+lvim.leader = ','
+
+lvim.foldmethod = "syntax"
