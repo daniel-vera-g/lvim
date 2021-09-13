9afba30c676634f691845e542dcf094e99edc007
danielVg <danielveragi@gmail.com>
2021-09-13 10:37:32 +0200

    Don't get into ex mode by mistake

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -6,3 +6,6 @@ vim.api.nvim_set_keymap('n', 'Y', 'yg_', {noremap=true})
 -- Some spelling keymaps
 vim.api.nvim_set_keymap('n', '<F8>', ':setlocal spelllang=de spellfile=~/.config/lvim/spell/de.utf-8.add spell<CR>', {noremap=true})
 vim.api.nvim_set_keymap('n', '<F7>', ':setlocal nospell<CR>', {noremap=true})
+
+-- no :ex mode
+vim.cmd("nnoremap Q <nop>")
