a5c4ef29be5a08bf434c328d3d047f099dc24c06
danielVg <danielveragi@gmail.com>
2021-09-18 12:30:29 +0200

    Feat: Format markdown with prettier

--- lua/user/languages/markdown.lua
+++ lua/user/languages/markdown.lua
@@ -1,4 +1,9 @@
 -- Don't check spell on default
 lvim.autocommands.custom_groups = {
-  { "FileType", "markdown", "setlocal nospell"}
+	{ "FileType", "markdown", "setlocal nospell" },
 }
+
+-- TODO use here `lvim.lang` when implemented
+-- See: #1554 & #1249
+local null_ls = require "null-ls"
+null_ls.register { sources = { null_ls.builtins.formatting.prettier } }
