9e58d615f357a368c3b451f4eb4af14a79b95072
danielVg <danielveragi@gmail.com>
2021-09-30 11:37:53 +0200

    Eslint and prettier config

--- lua/user/languages/markdown.lua
+++ lua/user/languages/markdown.lua
@@ -5,5 +5,5 @@ lvim.autocommands.custom_groups = {
 
 -- TODO use here `lvim.lang` when implemented
 -- See: #1554 & #1249
-local null_ls = require "null-ls"
-null_ls.register { sources = { null_ls.builtins.formatting.prettier } }
+local null_ls = require("null-ls")
+null_ls.register({ sources = { null_ls.builtins.formatting.prettier } })
