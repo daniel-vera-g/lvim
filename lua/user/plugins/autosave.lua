c5c20f9ee4b2e0dd536f485499d284a3061eeb05
danielVg <danielveragi@gmail.com>
2021-10-03 10:25:43 +0200

    Autosave formatting

--- lua/user/plugins/autosave.lua
+++ lua/user/plugins/autosave.lua
@@ -1,15 +1,13 @@
 local autosave = require("autosave")
 
 -- See: https://github.com/Pocco81/AutoSave.nvim#general
-autosave.setup(
-    {
-        enabled = true,
-        execution_message = "",
-        events = {"InsertLeave", "TextChanged"},
-        write_all_buffers = false,
-        on_off_commands = true,
-        clean_command_line_interval = 0,
-    }
-)
+autosave.setup({
+	enabled = true,
+	execution_message = "",
+	events = { "InsertLeave" },
+	write_all_buffers = false,
+	on_off_commands = true,
+	clean_command_line_interval = 0,
+})
 
 return autosave
