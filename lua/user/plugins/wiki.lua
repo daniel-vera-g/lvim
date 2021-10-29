356abcc2e1b6265f0c721fa0743c5651fa8ded07
danielVg <danielveragi@gmail.com>
2021-10-29 10:19:39 +0200

    Toggling wiki links

--- lua/user/plugins/wiki.lua
+++ lua/user/plugins/wiki.lua
@@ -1,3 +1,8 @@
 vim.g.wiki_root = "~/foam-knowledge"
 vim.g.wiki_filetypes = { "md" }
 vim.g.wiki_link_extension = ".md"
+vim.g.wiki_link_toggles = {
+	wiki = function(url, text)
+		return "[[" + text + "]]"
+	end,
+}
