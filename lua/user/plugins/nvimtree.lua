085c24eb0627d02a0f36218893eacaebaf31ea58
danielVg <danielveragi@gmail.com>
2021-11-03 14:15:24 +0100

    Nvimtree depreceates fix

--- lua/user/plugins/nvimtree.lua
+++ lua/user/plugins/nvimtree.lua
@@ -1,5 +1,4 @@
 lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.nvimtree.show_icons.git = 0
-lvim.builtin.nvimtree.hide_dotfiles = 0
 lvim.builtin.nvimtree.nvim_tree_disable_netrw = 0
 lvim.builtin.nvimtree.nvim_tree_hijack_netrw = 0
