b59dc926112d77bd4ae1d9ca12078a4cf5a9fb52
danielVg <danielveragi@gmail.com>
2021-12-04 23:52:16 +0100

    chore: remove as depreceated

--- lua/user/plugins/nvimtree_config.lua
+++ lua/user/plugins/nvimtree_config.lua
@@ -2,4 +2,3 @@ lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.nvimtree.nvim_tree_disable_netrw = 0
 lvim.builtin.nvimtree.nvim_tree_hijack_netrw = 0
-lvim.builtin.nvimtree.hide_dotfiles = 0
