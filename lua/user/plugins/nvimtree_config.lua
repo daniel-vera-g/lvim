daa72ea30d51c472ce76396f0f9ffe78e521f5d8
danielVg <danielveragi@gmail.com>
2021-12-17 17:31:18 +0100

    nvimtree: Show git changes

--- lua/user/plugins/nvimtree_config.lua
+++ lua/user/plugins/nvimtree_config.lua
@@ -2,3 +2,11 @@ lvim.builtin.nvimtree.setup.view.side = "right"
 lvim.builtin.nvimtree.show_icons.git = 0
 lvim.builtin.nvimtree.nvim_tree_disable_netrw = 0
 lvim.builtin.nvimtree.nvim_tree_hijack_netrw = 0
+
+require("nvim-tree").setup({
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+})
