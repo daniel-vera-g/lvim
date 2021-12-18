770c8f0b77e355c4114341677efc4fefbbea6c80
danielVg <danielveragi@gmail.com>
2021-12-18 16:48:24 +0100

    go: F10 to run go tests

--- lua/user/plugins/vim_go_config.lua
+++ lua/user/plugins/vim_go_config.lua
@@ -2,3 +2,6 @@
 -- In general, use neovim lsp for lsp and vim_go for go related features(Run, Compile,..)
 vim.g.go_def_mapping_enabled = 0
 vim.g.go_doc_keywordprg_enabled = 0
+
+-- Map F10 to go test
+vim.api.nvim_set_keymap("n", "<F10>", "<cmd>GoTest<cr>", { noremap = true, silent = true })
