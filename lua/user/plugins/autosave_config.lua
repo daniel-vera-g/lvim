ee814306b733338d77d2b412deccdda8d3b72f15
danielVg <danielveragi@gmail.com>
2021-12-18 16:48:10 +0100

    autosave: Some helful config

--- lua/user/plugins/autosave_config.lua
+++ lua/user/plugins/autosave_config.lua
@@ -5,9 +5,10 @@ autosave.setup({
 	enabled = true,
 	execution_message = "",
 	events = { "InsertLeave" },
-	write_all_buffers = false,
+	write_all_buffers = true,
 	on_off_commands = true,
 	clean_command_line_interval = 0,
+	debounce_delay = 500,
 })
 
 return autosave
