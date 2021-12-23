1670eae1b880d81ccf5585753ae769187c075995
Daniel VG <danielveragi@gmail.com>
2021-12-23 15:53:42 +0100

    Better keymaps (#8)
    * Keymap: Refactor and smoother resizing of windows
    
    * Keymap: Better indenting

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -1,17 +1,15 @@
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
 -- General keybindings
 -- keymappings [view all the defaults by pressing <leader>Lk]
 
 -- Yank remaining line
-vim.api.nvim_set_keymap("n", "Y", "yg_", { noremap = true })
+keymap("n", "Y", "yg_", { noremap = true })
 
 -- Some spelling keymaps
-vim.api.nvim_set_keymap("n", "<F7>", ":setlocal nospell<CR>", { noremap = true })
-vim.api.nvim_set_keymap(
-	"n",
-	"<F8>",
-	":setlocal spelllang=de spellfile=~/.config/lvim/spell/de.utf-8.add spell<CR>",
-	{ noremap = true }
-)
+keymap("n", "<F7>", ":setlocal nospell<CR>", { noremap = true })
+keymap("n", "<F8>", ":setlocal spelllang=de spellfile=~/.config/lvim/spell/de.utf-8.add spell<CR>", { noremap = true })
 
 -- no :ex mode
 vim.cmd("nnoremap Q <nop>")
@@ -27,3 +25,17 @@ lvim.builtin.which_key.mappings["rg"] = {
 	"<cmd>:lua require'telescope.builtin'.live_grep({ vimgrep_arguments = { 'rg', '--hidden','--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-u', '-g', '!yarn.lock', '-g', '!.git', '-g', '!node_modules', '-g', '!.next' } })<CR>",
 	"Find Text",
 }
+
+-- Keep the yanked stuff in the clipboard for pasting
+-- Seen on ChrisAtMachine: https://youtu.be/435-amtVYJ8
+keymap("v", "p", '"_dP', opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+keymap("n", "<C-Down>", ":resize -2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
