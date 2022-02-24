5c9989c0df72d49688c41a66c35214be9d9f232f
danielVg <danielveragi@gmail.com>
2022-02-24 08:51:38 +0100

    refactor: create "appearance" file for color schemes & co

--- lua/user/lvim.lua
+++ lua/user/lvim.lua
@@ -1,31 +1,5 @@
-local helpers = require("user.helpers.helper")
-local get_basename = helpers.get_cwd
-local autosave = require("autosave")
-
 lvim.format_on_save = true
 lvim.lint_on_save = true
-
-if get_basename() == "foam-knowledge" then
-	lvim.colorscheme = "catppuccin"
-	autosave.setup({
-		conditions = {
-			filetype_is_not = { "markdown" },
-		},
-	})
-else
-	lvim.colorscheme = "tokyonight"
-end
-
--- TODO use in summer or when sun is shining ☀️
--- if is_light() and get_cwd() ==  then
--- 	vim.o.background = "light"
--- 	lvim.colorscheme = "PaperColor"
--- 	-- lvim.colorscheme = "codemonkey"
--- else
--- 	vim.o.background = "dark"
--- 	vim.g.tokyonight_style = "storm"
--- 	lvim.colorscheme = "tokyonight"
--- end
 
 lvim.encoding = "utf-8"
 lvim.fileencoding = "utf-8"
@@ -37,3 +11,5 @@ lvim.foldmethod = "syntax"
 lvim.builtin.dashboard.active = true
 lvim.builtin.dap.active = true
 lvim.lsp.diagnostics.virtual_text = true
+
+require("user.appearance")
