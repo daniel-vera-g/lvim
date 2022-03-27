c0a6e057374a0aa644f4faa4d629d28b63449e8f
danielVg <danielveragi@gmail.com>
2022-03-27 22:31:21 +0200

    colorscheme: customizations

--- lua/user/appearance.lua
+++ lua/user/appearance.lua
@@ -3,24 +3,28 @@ local helpers = require("user.helpers.helper")
 
 -- Colorscheme customisations
 
-local get_basename = helpers.get_cwd
-if get_basename() == "foam-knowledge" then
-	-- Format when leaving the buffer to not mess with markdown lists when swtiching between normal/insert mode
-	autosave.setup({
-		events = { "BufEnter" },
-	})
-end
+-- Set to true to use the dark theme
+local activate_dark = false
 
 -- Use when the sun is shining ☀️
 local is_light = helpers.is_light
-local deactivate_light = true
-if is_light() and not deactivate_light then
+if is_light() and activate_dark then
 	vim.o.background = "light"
 	lvim.colorscheme = "PaperColor"
 else
 	vim.o.background = "dark"
 	require("colorbuddy").colorscheme("cobalt2")
 	lvim.colorscheme = "cobalt2"
+end
+
+local get_basename = helpers.get_cwd
+if get_basename() == "foam-knowledge" then
+	require("monokai").setup({})
+	lvim.colorscheme = "monokai"
+	-- Format when leaving the buffer to not mess with markdown lists when swtiching between normal/insert mode
+	autosave.setup({
+		events = { "BufEnter" },
+	})
 end
 
 -- Workarround, as vim.api does not seem as stable for highlighting
