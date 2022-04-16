6bb815079a72ea8c5a09fe3aecf2e5c8d1819de1
danielVg <danielveragi@gmail.com>
2022-04-16 18:12:52 +0200

    zenmode: update config

--- lua/user/plugins/zen_mode_config.lua
+++ lua/user/plugins/zen_mode_config.lua
@@ -1,20 +1,20 @@
-require("zen-mode").setup {
-  window = {
-    width = .70
-  },
-  plugins = {
-    options = {
-      enabled = true,
-      ruler = false, -- disables the ruler text in the cmd line area
-      showcmd = false, -- disables the command in the last line of the screen
-    },
-    twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
-    gitsigns = { enabled = true }, -- disables git signs
-    tmux = { enabled = true }, -- disables the tmux statusline
-    -- this will change the font size on kitty when in zen mode
-    kitty = {
-      enabled = true,
-      font = "+4", -- font size increment
-    },
-  }
-}
+require("zen-mode").setup({
+	window = {
+		width = 0.70,
+	},
+	plugins = {
+		options = {
+			enabled = true,
+			ruler = true, -- disables the ruler text in the cmd line area
+			showcmd = false, -- disables the command in the last line of the screen
+		},
+		twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+		gitsigns = { enabled = true }, -- disables git signs
+		tmux = { enabled = true }, -- disables the tmux statusline
+		-- this will change the font size on kitty when in zen mode
+		kitty = {
+			enabled = true,
+			font = "+4", -- font size increment
+		},
+	},
+})
