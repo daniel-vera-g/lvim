303bba51ab850dee9ac0b95f340f5ac1288e3f6d
danielVg <danielveragi@gmail.com>
2022-05-26 22:45:15 +0200

    bullets: note about mapping

--- lua/user/plugins/bullets_config.lua
+++ lua/user/plugins/bullets_config.lua
@@ -1,7 +1,10 @@
+-- NOTE: Apparently the silent mode for the <CR> mapping does not work properly in neovim?
+-- https://github.com/dkarter/bullets.vim/blob/master/plugin/bullets.vim#L971
+-- https://github.com/dkarter/bullets.vim/blob/master/doc/bullets.txt#L277
+-- https://github.com/neovim/neovim/issues/17758
 vim.g.bullets_enabled_file_types = {
-			'markdown',
-			'text',
-			'gitcommit',
-			'md'
+	"markdown",
+	"text",
+	"gitcommit",
+	"md",
 }
-
