ad648b16616c675f42439d24c6a759185bdcb32d
danielVg <danielveragi@gmail.com>
2022-06-02 11:48:05 +0200

    fix: dap currently not needed

--- lua/user/lvim.lua
+++ lua/user/lvim.lua
@@ -9,7 +9,7 @@ lvim.leader = ","
 lvim.foldmethod = "syntax"
 
 lvim.builtin.alpha.active = true
-lvim.builtin.dap.active = true
+lvim.builtin.dap.active = false
 lvim.lsp.diagnostics.virtual_text = true
 
 --- Silence deprecation notice
