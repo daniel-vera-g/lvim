c941d959789eb5dc240d9bb25b8173365e089c58
danielVg <danielveragi@gmail.com>
2022-09-11 11:31:30 +0200

    misc: disable auto saving

--- lua/user/plugins/autosave_config.lua
+++ lua/user/plugins/autosave_config.lua
@@ -2,7 +2,7 @@ local autosave = require("autosave")
 
 -- See: https://github.com/Pocco81/AutoSave.nvim#general
 autosave.setup({
-	enabled = true,
+	enabled = false,
 	execution_message = "",
 	-- Full list of events: https://tech.saigonist.com/b/code/list-all-vim-script-events.html
 	-- Example: Save, when changing the buffer. Otherwise, save manually to f.ex format the buffer.
