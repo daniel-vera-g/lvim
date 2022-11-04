34164cdd5f650aa5ff8ec433e48bb34b76d3a66a
danielVg <danielveragi@gmail.com>
2022-11-04 14:00:28 +0100

    chore: formatting

--- lua/user/helpers/helper.lua
+++ lua/user/helpers/helper.lua
@@ -1,19 +1,19 @@
 -- If it's between 9am and 4:30pm, use the light theme
 local function is_light()
-	local hour = os.date("%H%M")
-	return tonumber(hour) >= 900 and tonumber(hour) < 1630
+  local hour = os.date("%H%M")
+  return tonumber(hour) >= 900 and tonumber(hour) < 1630
 end
 
 -- Get current directory name
 local function get_basename()
-	local cwd = vim.fn.getcwd()
-	if cwd == "" then
-		cwd = "~"
-	end
-	return string.match(cwd, "/(%w+[-]*%w+)$")
+  local cwd = vim.fn.getcwd()
+  if cwd == "" then
+    cwd = "~"
+  end
+  return string.match(cwd, "/(%w+[-]*%w+)$")
 end
 
 return {
-	is_light = is_light,
-	get_cwd = get_basename,
+  is_light = is_light,
+  get_cwd = get_basename,
 }
