797aa8c8fd003ec737cfcc826be5d5f1ee539e26
danielVg <danielveragi@gmail.com>
2022-11-04 14:10:43 +0100

    fix: lag with j/k on visual mode

--- lua/user/keybindings.lua
+++ lua/user/keybindings.lua
@@ -33,8 +33,6 @@ keymap("v", ">", ">gv", opts)
 -- Exit insert mode fast
 keymap("i", "jk", "<esc>", { noremap = true, silent = true })
 keymap("i", "kj", "<esc>", { noremap = true, silent = true })
-keymap("v", "jk", "<esc>", { noremap = true, silent = true })
-keymap("v", "kj", "<esc>", { noremap = true, silent = true })
 
 ---------------------
 -- Which Key bindings
