5e8db89baeec6a20e556689b3c11cb928bb53051
danielVg <danielveragi@gmail.com>
2022-12-15 20:49:58 +0100

    Use reload 👀

--- config.lua
+++ config.lua
@@ -1,11 +1,11 @@
 -- Vim built in config
-require("user.config")
+reload("user.config")
 
-require("user.keybindings")
-require("user.plugins")
-require("user.formatters")
-require("user.linters")
-require("user.lsp")
+reload("user.keybindings")
+reload("user.plugins")
+reload("user.formatters")
+reload("user.linters")
+reload("user.lsp")
 
 -- Lunarvim configuration
-require("user.lvim")
+reload("user.lvim")
