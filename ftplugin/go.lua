e8a5a4bdc8774a40d88fccff201a0be56381967f
danielVg <danielveragi@gmail.com>
2022-12-15 20:48:03 +0100

    go: dap debug setup

--- ftplugin/go.lua
+++ ftplugin/go.lua
@@ -1,3 +1,16 @@
 -- ~/.config/lvim/ftplugin/go.lua
--- local dap_install = require("dap-install")
--- dap_install.config("go_delve", {})
+local dap = require("dap")
+dap.adapters.go = {
+	type = "executable",
+	command = "go-debug-adapter",
+}
+dap.configurations.go = {
+	{
+		type = "go",
+		name = "Debug",
+		request = "launch",
+		showLog = false,
+		program = "${file}",
+		dlvToolPath = vim.fn.exepath("dlv"),
+	},
+}
