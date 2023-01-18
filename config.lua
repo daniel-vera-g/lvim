3272280be4173fe2ab29199137d66ffc9d52bfb2
danielVg <danielveragi@gmail.com>
2023-01-18 16:28:14 +0100

    cmp: add copilot

--- config.lua
+++ config.lua
@@ -9,3 +9,16 @@ reload("user.lsp")
 
 -- Lunarvim configuration
 reload("user.lvim")
+
+-- Further Plugins configuration to load
+table.insert(lvim.plugins, {
+	"zbirenbaum/copilot-cmp",
+	event = "InsertEnter",
+	requires = { "zbirenbaum/copilot.lua" },
+	config = function()
+		vim.defer_fn(function()
+			require("copilot").setup()
+			require("copilot_cmp").setup()
+		end, 100)
+	end,
+})
